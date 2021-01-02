9d78d53d50fcf91480b127c6bc4970c903983c15
mafler <matt-1001@hotmail.co.uk>
2021-01-02 12:43:54 +0000

    First attempt at solving problem

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Function to cache the matrix into the global environment 
 
 makeCacheMatrix <- function(x = matrix()) {
 
+matCache <- NULL
+set <-function(y= matrix()){ 
+   x <<- y
+   matCache <<- NULL
+   
+}
+get <- function() x 
+setSolve <- function(solve) matCache <<- solve
+getSolve <- function() matCache
+
+
+list(set = set, get = get, 
+     setSolve= setSolve,
+     getSolve = getSolve)
+
 }
 
 
-## Write a short comment describing this function
+## Checks to see if Matrix inverse is in cache if not
+## solves the matrix and then puts in cache
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x,...) {
         ## Return a matrix that is the inverse of 'x'
+m <- x$getSolve()
+ if(!is.null(m)){
+   
+   message("Getting Cache Data")
+   return(m)
+ }
+   
+   data <- x$get()
+   m <- solve(data)
+   x$setSolve(m)
+   m
+   
 }
+
+
+
+
